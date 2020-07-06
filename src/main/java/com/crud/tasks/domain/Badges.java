1359f61ac1f5813a75931a9469fc98a35746c609
hbartek230 <bhreniak@gmail.com>
2020-07-06 21:08:43 +0200

    Module 22.3 to code review

---
+++ src/main/java/com/crud/tasks/domain/Badges.java
@@ -0,0 +1,18 @@
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Badges {
+
+    @JsonProperty("votes")
+    private int votes;
+
+    @JsonProperty("attachmentsByType")
+    private AttachmentByType attachments;
+}
--- src/main/java/com/crud/tasks/domain/Trello.java
+++ src/main/java/com/crud/tasks/domain/Trello.java
@@ -10,17 +10,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class CreatedTrelloCard {
+public class Trello {
 
-    @JsonProperty("id")
-    private String id;
+    @JsonProperty("board")
+    private int board;
 
-    @JsonProperty("name")
-    private String name;
+    @JsonProperty("card")
+    private int card;
 
-    @JsonProperty("shortUrl")
-    private String shortUrl;
-
-    @JsonProperty("badges")
-    private Badges badges;
 }
